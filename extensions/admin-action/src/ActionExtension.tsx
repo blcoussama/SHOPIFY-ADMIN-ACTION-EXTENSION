a492cb4cab02035e57fd17e0cde785ea71e9fb5b
blcoussama <belcadioussama.eng@gmail.com>
2025-05-02 10:59:07 +0100

    Added the quantity slelector in the admin action and generates teh checkout link for it

--- extensions/admin-action/src/ActionExtension.tsx
+++ extensions/admin-action/src/ActionExtension.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import {
   reactExtension,
   useApi,
@@ -7,39 +7,42 @@ import {
   Button,
   Text,
   Select,
-} from '@shopify/ui-extensions-react/admin';
+  TextField,
+} from "@shopify/ui-extensions-react/admin";
 
 // The target used here must match the target used in the extension's toml file (./shopify.extension.toml)
-const TARGET = 'admin.product-details.action.render';
+const TARGET = "admin.product-details.action.render";
 
 export default reactExtension(TARGET, () => <App />);
 
 function App() {
   // The useApi hook provides access to several useful APIs like i18n, close, and data.
-  const {i18n, close, data} = useApi(TARGET);
-  console.log({data});
-  const [productTitle, setProductTitle] = useState('');
+  const { i18n, close, data } = useApi(TARGET);
+  console.log({ data });
+  const [productTitle, setProductTitle] = useState("");
   const [productVariants, setProductVariants] = useState([]);
   const [selectedVariant, setSelectedVariant] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+  const [checkoutLink, setCheckoutLink] = useState(null);
   // Use direct API calls to fetch data from Shopify.
   // See https://shopify.dev/docs/api/admin-graphql for more information about Shopify's GraphQL API
   useEffect(() => {
     (async function getProductInfo() {
       const getProductQuery = {
         query: `query Product($id: ID!) {
-          product(id: $id) {
-            title
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  title
-                }
-              }
-            }
-          }
-        }`,
-        variables: {id: data.selected[0].id},
+  product(id: $id) {
+    title
+    variants(first: 10) {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+  }
+}`,
+        variables: { id: data.selected[0].id },
       };
 
       const res = await fetch("shopify:admin/api/graphql.json", {
@@ -48,7 +51,7 @@ function App() {
       });
 
       if (!res.ok) {
-        console.error('Network error');
+        console.error("Network error");
       }
 
       const productData = await res.json();
@@ -57,23 +60,38 @@ function App() {
       setSelectedVariant(productData.data.product.variants.edges[0].node.id);
     })();
   }, [data.selected]);
+
+  const handleGenerateCheckoutLink = async () => {
+    const res = await fetch("api/generate-checkout-link", {
+      method: "POST",
+      body: JSON.stringify({
+        variantId: selectedVariant,
+        quantity: quantity,
+      }),
+    });
+    if (!res.ok) {
+      console.error("Network error");
+    }
+    const data = await res.json();
+    console.log(data);
+    if (data.checkoutLink) {
+      setCheckoutLink(data.checkoutLink);
+    }
+  };
   return (
     // The AdminAction component provides an API for setting the title and actions of the Action extension wrapper.
     <AdminAction
       primaryAction={
         <Button
-          onPress={() => {
-            console.log('saving');
-            close();
-          }}
+          onPress={handleGenerateCheckoutLink}
         >
-          Done
+          Generate Checkout Link
         </Button>
       }
       secondaryAction={
         <Button
           onPress={() => {
-            console.log('closing');
+            console.log("closing");
             close();
           }}
         >
@@ -83,18 +101,25 @@ function App() {
     >
       <BlockStack>
         {/* Set the translation values for each supported language in the locales directory */}
-        <Text fontWeight="bold">{i18n.translate('welcome', {target: TARGET})}</Text>
+        <Text fontWeight="bold">{i18n.translate("welcome", { TARGET })}</Text>
         <Text>Current product: {productTitle}</Text>
-        <Text>Selected Variant: {selectedVariant}</Text>
-        <Select 
+        <Text>Selected variant: {selectedVariant}</Text>
+        <Select
           options={productVariants.map((variant) => ({
             value: variant.node.id,
-            label: variant.node.title
-          }))} 
+            label: variant.node.title,
+          }))}
           onChange={(value) => setSelectedVariant(value)}
-          label='Select a Variant'
-          />
+          label="Select a variant"
+        />
+        <TextField
+          type="number"
+          value={quantity.toString()}
+          onChange={(value) => setQuantity(parseFloat(value))}
+          label="Quantity"
+        />
+        {checkoutLink && <Text>Checkout link: {checkoutLink}</Text>}
       </BlockStack>
     </AdminAction>
   );
-} 
+}
